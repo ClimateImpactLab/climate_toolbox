5a471e1eddb9063b1abda8b36e5ca803a8479fb7
jgerardsimcock <justin_simcock@yahoo.com>
2017-07-25 15:06:56 -0700

    Bump version: 0.1.1 → 0.1.2

--- climate_toolbox/__init__.py
+++ climate_toolbox/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='climate_toolbox',
-    version='0.1.1',
+    version='0.1.2',
     description="Tools for climate data wrangling",
     long_description=readme + '\n\n' + history,
     author="Justin Simcock",
