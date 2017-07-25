05682f6a72133e99f8dd7f3736bafc30957332ce
jgerardsimcock <justin_simcock@yahoo.com>
2017-07-24 18:29:44 -0700

    update tests

--- climate_toolbox/climate_toolbox.py
+++ climate_toolbox/climate_toolbox.py
@@ -1,3 +1,473 @@
-# -*- coding: utf-8 -*-
+'''
+This file describes the process for computing weighted climate data
+'''
 
-"""Main module."""
+import xarray as xr
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+from scipy.ndimage import label
+from scipy.interpolate import griddata
+from six import string_types
+import itertools
+import toolz
+
+WEIGHTS_FILE = (
+    'GCP/spatial/world-combo-new/segment_weights/' +
+    'agglomerated-world-new_BCSD_grid_segment_weights_area_pop.csv')
+
+'''
+=================
+Private Functions
+=================
+'''
+
+
+def _fill_holes_xr(
+        ds,
+        varname,
+        broadcast_dims=('time',),
+        lon_name='lon',
+        lat_name='lat',
+        gridsize=0.25,
+        minlat=-85,
+        maxlat=85):
+    '''
+    Fill NA values inplace in a gridded dataset
+
+    Parameters
+    ----------
+
+    ds : xarray.Dataset
+        name of the dataset with variable to be modified
+
+    varname : str
+        name of the variable to be interpolated
+
+    broadcast_dims : tuple of strings, optional
+        tuple of dimension names to broadcast the interpolation step over
+        (default 'time')
+
+    lon_name : str, optional
+        name of the longitude dimension (default 'lon')
+
+    lat_name : str, optional
+        name of the latitude dimension (default 'lat')
+
+    gridsize : float, optional
+        size of the lat/lon grid. Important for creating a bounding box around
+        NaN regions (default 0.25)
+
+    minlat : float, optional
+        latitude below which no values will be interpolated (default -85)
+
+    minlon : float, optional
+        latitude above which no values will be interpolated (default 85)
+
+    '''
+    if isinstance(broadcast_dims, string_types):
+        broadcast_dims = (broadcast_dims, )
+
+    ravel_lons, ravel_lats = (
+        np.meshgrid(ds.coords[lon_name].values, ds.coords[lat_name].values))
+
+    # remove infinite values
+    ds[varname] = (
+        ds[varname]
+            .where(~np.isinf(ds[varname]))
+            .where(ds[varname] < 1e10))
+
+    for indexers in itertools.product(*tuple(
+            [range(len(ds.coords[c])) for c in broadcast_dims])):
+
+        slicer_dict = dict(zip(broadcast_dims, indexers))
+
+        slicer = tuple([
+                slicer_dict[c]
+                if c in broadcast_dims
+                else slice(None, None, None)
+                for c in ds[varname].dims])
+
+        sliced = ds[varname].values.__getitem__(slicer)
+
+        if not np.isnan(sliced).any():
+            continue
+
+        filled = _fill_holes(
+            var=np.ma.masked_invalid(sliced),
+            lat=ravel_lats,
+            lon=ravel_lons,
+            gridsize=0.25,
+            minlat=-85,
+            maxlat=85)
+
+        ds[varname][slicer_dict] = filled
+
+
+def _fill_holes(var, lat, lon, gridsize=0.25, minlat=-85, maxlat=85):
+    '''
+    Interpolates the missing values between points on grid
+
+    Parameters
+    ----------
+    var: masked np.array
+        array of climate values
+
+    lat: masked np.array
+        array of latitude values
+
+    lon: masked np. array
+        array of longitude values
+
+    gridsize: int
+        corresponds to degrees on the grid for climate data
+
+    minlat: int
+        corresponds to min latitude values to include. Used to remove poles
+
+    maxlat: int
+        corresponds to max lat values to include. Used to remove poles
+
+
+    '''
+    # fill the missing value regions by linear interpolation
+    # pass if no missing values
+    if not np.ma.is_masked(var):
+        return var
+    # or the missing values are only in polar regions
+    if not var.mask[20: -20, :].any():
+        return var
+
+    # fill the holes
+    var_filled = var[:]
+    missing = np.where((var.mask) & (lat > minlat) & (lat < maxlat))
+    mp = np.zeros(var.shape)
+    mp[missing] = 1
+    ptch, n_ptch = label(mp)
+
+    for p in range(1, n_ptch+1):
+
+        ind_ptch = np.where(ptch == p)
+        lat_ptch = lat[ind_ptch]
+        lon_ptch = lon[ind_ptch]
+
+        ind_box = np.where(
+                (lat <= np.max(lat_ptch)+gridsize) &
+                (lat >= np.min(lat_ptch)-gridsize) &
+                (lon <= np.max(lon_ptch)+gridsize) &
+                (lon >= np.min(lon_ptch)-gridsize))
+
+        var_box = var[ind_box]
+        lat_box = lat[ind_box]
+        lon_box = lon[ind_box]
+        not_missing = np.where(var_box.mask == False)
+        points = np.column_stack([lon_box[not_missing], lat_box[not_missing]])
+        values = var_box[var_box.mask == False]
+        var_filled[ind_box] = griddata(
+                points,
+                values,
+                (lon_box, lat_box),
+                method='linear')
+
+    return var_filled
+
+
+def _standardize_longitude_dimension(ds, lon_names=['lon', 'longitude']):
+    '''
+    Rescales the lat and lon coordinates to ensure lat is within (-90,90)
+    and lon is within (-180, 180). Renames coordinates
+    from lon to longitude and from lat to latitude. Sorts any new
+    rescaled coordinated.
+
+    Parameters
+    ----------
+    ds: xarray.DataSet
+
+    Returns
+    -------
+    ds: xarray.DataSet
+
+    .. note:: this will be unnecessary if we standardize inputs. We can
+    scale the longitude dim to between (-180, 180)
+
+    '''
+
+    coords = np.array(ds.coords.keys())
+
+    assert len(coords[np.in1d(coords, lon_names)]) == 1
+    _lon_coord = coords[np.in1d(coords, ['longitude', 'lon'])][0]
+
+    ds = ds.rename({_lon_coord: '_longitude'})
+
+    # Adjust lat and lon to make sure they are within (-90, 90) and (-180, 180)
+    ds['_longitude_adjusted'] = (
+        (ds._longitude - 360)
+            .where(ds._longitude > 180)
+            .fillna(ds._longitude))
+
+    # reassign the new coords to as the main lon coords
+    ds = (ds
+        .swap_dims({'_longitude': '_longitude_adjusted'})
+        .reindex({'_longitude_adjusted': sorted(ds._longitude_adjusted)}))
+
+    if '_longitude' in ds.coords:
+        ds = ds.drop('_longitude')
+
+    ds = ds.rename({'_longitude_adjusted': _lon_coord})
+
+    return ds
+
+
+@toolz.memoize
+def _prepare_spatial_weights_data(weights_file=WEIGHTS_FILE):
+    '''
+    Rescales the pix_cent_x colum values
+
+    Parameters
+    ----------
+    weights_file: str
+        location of file used for weighting
+
+
+    .. note:: unnecessary if we can standardize our input
+    '''
+
+    api = datafs.get_api()
+    archive = api.get_archive(weights_file)
+
+    with archive.open('r') as f:
+        df = pd.read_csv(f)
+
+    # Re-label out-of-bounds pixel centers
+    df.set_value((df['pix_cent_x'] == 180.125), 'pix_cent_x', -179.875)
+
+    #probably totally unnecessary
+    df.drop_duplicates()
+    df.index.names = ['reshape_index']
+
+    df.rename(
+        columns={'pix_cent_x': 'lon', 'pix_cent_y': 'lat'},
+        inplace=True)
+
+    return df
+
+
+def _reindex_spatial_data_to_regions(ds, df):
+    '''
+    Reindexes spatial and segment weight data to regions
+
+    Enables region index-based math operations
+
+    Parameters
+    ----------
+    ds: xarray Dataset
+    df: pandas DataFrame
+
+    Returns
+    -------
+    Xarray DataArray
+
+
+    '''
+    res = ds.sel_points(
+        'reshape_index',
+        lat=df.lat.values,
+        lon=df.lon.values)
+
+    return res
+
+
+def _aggregate_reindexed_data_to_regions(
+        ds,
+        variable,
+        aggwt,
+        agglev,
+        weights,
+        backup_aggwt='areawt'):
+    '''
+    Performs weighted avg for climate variable by region
+
+    Parameters
+    ----------
+
+    ds: xarray.DataArray
+
+    variable: str
+        name of the data variable
+
+    aggwt: str
+        variable to weight by (i.e popwt, areawt, cropwt)
+
+    agglev: str
+        indicates which regional id scheme to select in the dataframe
+
+    weight: pd.DataFrame
+        pandas DataFrame of weights
+
+    backup_aggwt: str, optional
+        aggregation weight to use in regions with no aggwt data (default
+        'areawt')
+
+    '''
+
+    ds.coords[agglev] = xr.DataArray(
+                weights[agglev].values,
+                dims={'reshape_index': weights.index.values})
+
+    # format weights
+    ds[aggwt] = xr.DataArray(
+                weights[aggwt].values,
+                dims={'reshape_index': weights.index.values})
+
+    ds[aggwt] = ds[aggwt].where(ds[aggwt] > 0).fillna(weights[backup_aggwt].values)
+
+    weighted = xr.Dataset({
+        variable: (
+            (ds[variable]*ds[aggwt])
+                .groupby(agglev)
+                .sum(dim='reshape_index') /
+            ds[aggwt]
+                .groupby(agglev)
+                .sum(dim='reshape_index'))})
+
+    return weighted
+
+
+'''
+================
+Public Functions
+================
+'''
+
+def load_bcsd(fp, varname, lon_name='lon', broadcast_dims=('time',)):
+    '''
+    Read and prepare climate data
+
+    After reading data, this method also fills NA values using linear
+    interpolation, and standardizes longitude to -180:180
+
+    Parameters
+    ----------
+    fp: str
+        File path to dataset
+
+    varname: str
+        Variable name to be read
+
+    lon_name : str, optional
+        Name of the longitude dimension (defualt selects from ['lon' or
+        'longitude'])
+
+    Returns
+    -------
+    xr.Dataset
+         xarray dataset loaded into memory
+    '''
+
+    if lon_name is not None:
+        lon_names = [lon_name]
+
+    if hasattr(fp, 'sel_points'):
+        ds = fp
+
+    else:
+        with xr.open_dataset(fp) as ds:
+            ds.load()
+
+    _fill_holes_xr(ds, varname, broadcast_dims=broadcast_dims)
+    return _standardize_longitude_dimension(ds, lon_names=lon_names)
+
+
+def load_baseline(fp, varname, lon_name='lon', broadcast_dims=None):
+    '''
+    Read and prepare climate data
+
+    After reading data, this method also fills NA values using linear
+    interpolation, and standardizes longitude to -180:180
+
+    Parameters
+    ----------
+    fp: str
+        File path to dataset
+
+    varname: str
+        Variable name to be read
+
+    lon_name : str, optional
+        Name of the longitude dimension (defualt selects from ['lon' or
+        'longitude'])
+
+    Returns
+    -------
+    xr.Dataset
+         xarray dataset loaded into memory
+    '''
+
+    if lon_name is not None:
+        lon_names = [lon_name]
+
+    if broadcast_dims is None:
+        broadcast_dims = tuple([])
+
+    with xr.open_dataset(fp) as ds:
+        ds.load()
+
+    if 'lat' in ds.data_vars:
+        ds = ds.set_coords('lat')
+        ds = ds.swap_dims({'nlat': 'lat'})
+
+    if 'lon' in ds.data_vars:
+        ds = ds.set_coords('lon')
+        ds = ds.swap_dims({'nlon': 'lon'})
+
+    _fill_holes_xr(ds, varname, broadcast_dims=broadcast_dims)
+    return _standardize_longitude_dimension(ds, lon_names=lon_names)
+
+def weighted_aggregate_grid_to_regions(
+        ds,
+        variable,
+        aggwt,
+        agglev,
+        weights=None):
+    '''
+    Computes the weighted reshape of gridded data
+
+    Parameters
+    ----------
+    ds : xr.Dataset
+        xarray Dataset to be aggregated. Must have 'lat' and 'lon' in the
+        coordinates.
+
+    variable : str
+        name of the variable to be aggregated
+
+    aggwt : str
+        Weighting variable (e.g. 'popwt', 'areawt'). This must be a column name
+        in the weights file.
+
+    agglev : str
+        Target regional aggregation level (e.g. 'ISO', 'hierid'). This must be
+        a column name in the weights file.
+
+    weights : str, optional
+        Regional aggregation weights (default agglomerated-world-new BCSD
+        segment weights)
+
+    Returns
+    -------
+    ds: xr.Dataset
+        weighted and averaged dataset based on agglev
+    '''
+
+    if weights is None:
+        weights = _prepare_spatial_weights_data()
+
+    ds = _reindex_spatial_data_to_regions(ds, weights)
+    ds = _aggregate_reindexed_data_to_regions(
+        ds,
+        variable,
+        aggwt,
+        agglev,
+        weights)
+
+    return ds
--- tests/test_climate_toolbox.py
+++ tests/test_climate_toolbox.py
@@ -4,10 +4,115 @@
 """Tests for `climate_toolbox` package."""
 
 import pytest
+
 from click.testing import CliRunner
 
-from climate_toolbox import climate_toolbox
+from climate_toolbox import climate_toolbox as ctb
 from climate_toolbox import cli
+
+import numpy as np
+import pandas as pd
+import pytest
+import xarray as xr
+import itertools
+
+
+#python utils
+
+#create pytest resource to be used across tests
+@pytest.fixture
+def clim_data():
+    '''
+    Generate fake climate data to do tests on
+    '''
+    np.random.seed(42)
+    temp = np.random.rand(720, 1440,365)*100
+    lat = np.arange(-89.875,90,0.25)
+    lon = np.arange(0.125, 360., .25)
+    time = pd.date_range(start=pd.datetime(2000, 1, 1), periods=365)
+    ds = xr.Dataset({'temperature': (['lat', 'lon', 'time'],  temp)}, 
+                    coords={'lon':  lon,
+                            'lat':  lat,
+                            'time':time})
+
+    return ds
+
+
+def test_clim_data(clim_data):
+
+    assert clim_data.isnull().any() == False
+
+
+@pytest.fixture
+def make_holes(clim_data):
+
+    tmp = clim_data['temperature'].values
+    array = np.random.randint(0,719, 1)
+    tmp[array] = np.nan
+
+    clim_data['temperature'].values = tmp
+
+    return clim_data
+
+
+#create pytest resource to be used acress tessts
+def test_fill_holes(make_holes):
+
+    assert make_holes.isnull().any() == True
+    ctb._fill_holes_xr(make_holes, 'temperature')
+
+    assert make_holes.isnull().any() == False
+
+@pytest.fixture
+def weights():
+
+    df = pd.DataFrame()
+    lat = np.random.choice(np.arange(-89.875,90,0.25), 1000)
+    lon = np.random.choice(np.arange(0.125, 360., .25), 1000)
+    df['lat'] = lat
+    df['lon'] = lon
+    
+    df['areawt'] = np.random.random(len(df['lon']))
+    tmp = np.random.random(len(df['lon']))
+    tmp[::5] = np.nan
+    df['popwt'] = tmp
+    df['hierid'] = np.random.choice(np.arange(1,250), len(lat))
+    mapping = {h: np.random.choice(np.arange(1,20)) for h in df['hierid'].values}
+    df['ISO'] = [mapping[i] for i in df['hierid']]
+    df.index.names = ['reshape_index']
+
+    return df
+
+
+def test_reindex_spatial_weights(clim_data, weights):
+
+    assert clim_data.isnull().any() == False
+
+    ds = ctb._reindex_spatial_data_to_regions(clim_data, weights)
+
+    assert ds.temperature.shape == (len(ds['lon']), len(ds['time']))
+    assert 'reshape_index' in ds.dims
+
+
+
+
+def test_weighting(clim_data, weights):
+
+    assert np.isnan(weights['popwt'].values).any() == True
+    ds = ctb._reindex_spatial_data_to_regions(clim_data, weights)
+    assert ds.temperature.isnull().any() == False
+
+    wtd = ctb._aggregate_reindexed_data_to_regions(ds, 'temperature', 'popwt', 'ISO', weights)
+    assert wtd.temperature.isnull().any() == False
+
+    wtd = ctb._aggregate_reindexed_data_to_regions(ds, 'temperature', 'areawt', 'ISO', weights)
+    assert wtd.temperature.isnull().any() == False
+
+
+
+
+
+
 
 @pytest.fixture
 def response():
@@ -34,3 +139,6 @@ def test_command_line_interface():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
+
+
+
