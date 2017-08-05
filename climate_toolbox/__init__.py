c11d3120921ba209694fcc6ec0adbe6d3cedc240
Michael Delgado <delgado.michaelt@gmail.com>
2017-08-04 17:10:14 -0700

    Bump version: 0.1.2 → 0.1.3

--- climate_toolbox/__init__.py
+++ climate_toolbox/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='climate_toolbox',
-    version='0.1.2',
+    version='0.1.3',
     description="Tools for climate data wrangling",
     long_description=readme + '\n\n' + history,
     author="Justin Simcock",
