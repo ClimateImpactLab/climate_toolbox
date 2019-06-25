dfed606704c473727cfe5c0385e1662aa6d71274
Ana Trisovic <ana.trisovic@gmail.com>
2019-06-25 09:23:33 -0700

    remove load_baseline and cli

--- climate_toolbox/climate_toolbox.py
+++ climate_toolbox/climate_toolbox.py
@@ -405,56 +405,6 @@ Public Functions
 '''
 
 
-def load_baseline(fp, varname, lon_name='lon', broadcast_dims=None):
-    '''
-    Read and prepare climate data
-
-    After reading data, this method also fills NA values using linear
-    interpolation, and standardizes longitude to -180:180
-
-    Parameters
-    ----------
-    fp: str
-        File path or dataset
-
-    varname: str
-        Variable name to be read
-
-    lon_name : str, optional
-        Name of the longitude dimension (defualt selects from ['lon' or
-        'longitude'])
-
-    Returns
-    -------
-    xr.Dataset
-         xarray dataset loaded into memory
-    '''
-
-    if lon_name is not None:
-        lon_names = [lon_name]
-
-    if broadcast_dims is None:
-        broadcast_dims = tuple([])
-
-    if hasattr(fp, 'sel_points'):
-        ds = fp
-
-    else:
-        with xr.open_dataset(fp) as ds:
-            ds.load()
-
-    if 'lat' in ds.data_vars:
-        ds = ds.set_coords('lat')
-        ds = ds.swap_dims({'nlat': 'lat'})
-
-    if 'lon' in ds.data_vars:
-        ds = ds.set_coords('lon')
-        ds = ds.swap_dims({'nlon': 'lon'})
-
-    _fill_holes_xr(ds, varname, broadcast_dims=broadcast_dims)
-    return _standardize_longitude_dimension(ds, lon_names=lon_names)
-
-
 def weighted_aggregate_grid_to_regions(
         ds,
         variable,
