bb0a1713a3ad1317a928eafd604f6b1fa3a68a9f
Michael Delgado <delgado.michaelt@gmail.com>
2019-09-19 15:58:56 -0700

    actually this time?

--- climate_toolbox/__init__.py
+++ climate_toolbox/__init__.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-
 """Top-level package for climate_toolbox."""
+
+from __future__ import absolute_import
 
 __author__ = """Justin Simcock"""
 __email__ = 'jsimcock@rhg.com'
 __version__ = '0.1.5'
+
+import climate_toolbox.aggregations
+import climate_toolbox.geo
+import climate_toolbox.io
+import climate_toolbox.transformations
+import climate_toolbox.utils
+
+__all__ = [
+    'climate_toolbox.aggregations',
+    'climate_toolbox.geo',
+    'climate_toolbox.io',
+    'climate_toolbox.transformations',
+    'climate_toolbox.utils',
+]
